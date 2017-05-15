79bb3c3559a61d16a04b8e288fd46fd4fd555bab
starG <psk7142@naver.com>
2017-05-15 13:28:23 +0900

    ADD: input_data.py

---
+++ input_data.py
@@ -0,0 +1,101 @@
+import os
+from PIL import Image
+from progressbar import Percentage, Bar, SimpleProgress, ProgressBar
+
+batch_file_name_format = "data_batch_%d"
+folder_name = "cifar-10-batches-py"
+batches_meta_file_name = "batches.meta"
+test_batch_file_name = "test_batch"
+
+# progressBar widgets def
+widgets = ['Running: ', Percentage(), ' ',
+           Bar(marker='#', left='[', right=']'),
+           ' ', SimpleProgress()]
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def show_image(raw_data, img):
+    r, g, b = raw_data[:1024], raw_data[1024:1024 * 2], raw_data[1024 * 2:1024 * 3]
+    rgb_list = list(zip(r, g, b))
+    for x in range(32):
+        for y in range(32):
+            img.putpixel((x, y), rgb_list[y * 32 + x])
+    img.show()
+
+
+def open_meta_file():
+    print("Open meta file...")
+    meta = unpickle(os.path.join(folder_name, batches_meta_file_name))
+    label_names = meta[b'label_names']
+
+    for i in meta:
+        print("labels = %s" % label_names)
+
+    print()
+    return meta
+
+
+def open_train_batch_files():
+    print("Open train batch files...")
+
+    data_file = []
+    for batch_number in range(1,6):
+        #print("\nbatch:", batch_number)
+        batch = unpickle(os.path.join(folder_name, batch_file_name_format % batch_number))
+
+        # batch data inputs
+        batch_label = batch[b'batch_label']
+        data = batch[b'data']
+        file_names = batch[b'filenames']
+        labels = batch[b'labels']
+
+        # image gen
+        img_mode = 'RGB'
+        img_size = (32, 32)
+        img = Image.new(img_mode, img_size)
+
+        p_bar = ProgressBar(widgets=widgets, maxval=len(file_names)).start()
+        for i in range(len(file_names)):
+            #print("number: %4d, name: %s, label: %s" % (i, file_names[i], labels[i]))
+            # show_image(data[i], img)
+            p_bar.update(i)
+
+        data_file.append([batch_label, data, labels, file_names])
+        p_bar.finish()
+
+    print()
+    return data_file
+
+
+# test batch
+def open_image_batch():
+    print("Open test batch file...")
+    test_batch = unpickle(os.path.join(folder_name, test_batch_file_name))
+
+    batch_label = test_batch[b'batch_label']
+    data = test_batch[b'data']
+    file_names = test_batch[b'filenames']
+    labels = test_batch[b'labels']
+
+    test_data = [batch_label, data, file_names, labels]
+
+    p_bar = ProgressBar(widgets=widgets, maxval=len(file_names)).start()
+    for i in range(len(file_names)):
+        p_bar.update(i)
+        # print("number: %4d, name: %s, label: %s" % (i, file_names[i], labels[i]))
+    p_bar.finish()
+
+    print()
+    return test_data
+
+
+if __name__ == '__main__':
+    open_meta_file()
+    open_train_batch_files()
+    open_image_batch()
